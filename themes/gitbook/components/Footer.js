b8443a3848ef6e5cd9e287713c06799e6ed50c62
Leo <73161199+Armyh@users.noreply.github.com>
2024-09-28 13:31:56 +0800

    Update Footer.js

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -52,7 +52,7 @@ const Footer = ({ siteInfo }) => {
         <a
           href='https://github.com/tangly1024/NotionNext'
           className='underline text-gray-500 dark:text-gray-300'>
-          NotionNext {siteConfig('VERSION')}
+          NotionNext {siteConfig('')}
         </a>
       </div>
       {/* SEO title */}
