ac79fe188d50865e9d9d6f5a63d09c88925ab7f8
Leo <73161199+Armyh@users.noreply.github.com>
2025-08-31 23:54:10 +0800

    Update Footer.js

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -52,9 +52,9 @@ const Footer = ({ siteInfo }) => {
       <div className='text-xs font-serif'>
         Powered By{' '}
         <a
-          href='https://github.com/tangly1024/NotionNext'
+          href='https://boke.daxiang.de'
           className='underline text-gray-500 dark:text-gray-300'>
-          NotionNext {siteConfig('VERSION')}
+          DaXiang {siteConfig('VERSION')}
         </a>
       </div>
       {/* SEO title */}
